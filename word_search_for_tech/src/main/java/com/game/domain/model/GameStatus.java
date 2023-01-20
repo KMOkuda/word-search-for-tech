1326e7e553359eb93239bdb8d607d20410110352
KMOkuda <kyo.trainee.sys.202205@gmail.com>
2023-01-20 16:27:55 +0900

    リファクタリング中

--- word_search_for_tech/src/main/java/com/game/domain/model/GameStatus.java
+++ word_search_for_tech/src/main/java/com/game/domain/model/GameStatus.java
@@ -1,16 +1,9 @@
 package com.game.domain.model;
 
-import java.util.Date;
-
 import lombok.Data;
 
 @Data
 public class GameStatus{
-	int playId;
-	String mode;
-	String board;
-	String answerCode;
-	int point;
-	Date createdDate;
-	Date clearedDate;
+	int gameId;
+	
 }
