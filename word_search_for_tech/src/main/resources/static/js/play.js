8eb429157c5badd8cb95428e0efdda539c648a59
KMOkuda <kyo.trainee.sys.202205@gmail.com>
2023-02-05 22:19:24 +0900

    次やる事：回答コードの送信、すでに正解が確定している項目の受け渡し、canvas反映

--- word_search_for_tech/src/main/resources/static/js/play.js
+++ word_search_for_tech/src/main/resources/static/js/play.js
@@ -1,203 +1,213 @@
-				const canvasElm = document.getElementsByClassName( "board-canvas" )[0];
-				canvasElm.width = document.getElementsByClassName( "board-content" )[0].offsetWidth;
-				canvasElm.height = document.getElementsByClassName( "board-content" )[0].offsetHeight;
+var Point = class {
+
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	/**
+	 * @param {var} x
+	 */
+	set setX(x) {
+		this.x = x;
+	}
+
+	/**
+	 * @param {var} y
+	 */
+	set setY(y) {
+		this.y = y;
+	}
+
+	get getX() {
+		/* get構文 */
+		return this.x
+	}
+	get getY() {
+		/* get構文 */
+		return this.y
+	}
+}
+
+const Directions = {
+	NONE: 0,
+	RIGHT: 1,
+	DOWN: 2,
+	LEFT: 3,
+	UP: 4,
+	TOPRIGHT: 5,
+	DOWNRIGHT: 6,
+	DOWNLEFT: 7,
+	TOPLEFT: 8
+};
+
+/**
+ * 
+ * @param {var} fromId 
+ * @param {var} toId 
+ */
+
+function calcShortDict(fromId, toId) {
+	console.log("fromId, toId" + fromId, toId);
+	if (fromId + 1 == toId) {
+		return Directions.RIGHT;
+	}
+
+	if (fromId - 1 == toId) {
+		return Directions.LEFT;
+	}
 
-				console.log("width: " + canvasElm.width);
-				console.log("height: " + canvasElm.height);
+	if (fromId + boardWidth == toId) {
+		return Directions.DOWN;
+	}
 
-				//const canvasTop = canvasElm.offsetTop;
-				//const canvasLeft = canvasElm.offsetLeft;
-				//const canvasTop = window.pageYOffset;
-				//const canvasLeft = window.pageXOffset;
+	if (fromId - boardWidth == toId) {
+		return Directions.UP;
+	}
 
-				const canvasTop = canvasElm.getBoundingClientRect().top;
-				const canvasLeft = canvasElm.getBoundingClientRect().left;
+	if (fromId + boardWidth + 1 == toId) {
+		return Directions.DOWNRIGHT;
+	}
 
+	if (fromId + boardWidth - 1 == toId) {
+		return Directions.DOWNLEFT;
+	}
 
-				console.log("canvasTop: " + canvasTop);
-				console.log("canvasLeft: " + canvasLeft);
+	if (fromId - boardWidth + 1 == toId) {
+		return Directions.TOPRIGHT;
+	}
 
-				var drawFromY;
-				var drawFromX;
-				var drawToY;
-				var drawToX;
+	if (fromId - boardWidth - 1 == toId) {
+		return Directions.TOPLEFT;
+	}
 
-				//'use strict';
+	return Directions.NONE;
+}
 
+function isDraggable(fromId, toId) {
+	if (Math.floor(fromId / boardWidth) == Math.floor(toId / boardWidth)) {
+		return true;
+	}
 
-				function startDraw(e) {
+	if (Math.abs(toId - fromId) % boardWidth == 0) {
+		return true;
+	}
+	if (Math.abs(toId - fromId) % (boardWidth - 1) == 0) {
+		return true;
 
-					e.preventDefault();
+	}
+	if (Math.abs(toId - fromId) % (boardWidth + 1) == 0) {
+		return true;
+	}
+}
 
-					console.log("started");
 
-					var originX = e.touches[0].pageX;
-					var originY = e.touches[0].pageY;
+const canvasElm = document.getElementsByClassName("board-canvas")[0];
+canvasElm.width = document.getElementsByClassName("board-content")[0].offsetWidth;
+canvasElm.height = document.getElementsByClassName("board-content")[0].offsetHeight;
 
-					var elmFrom = document.elementFromPoint(originX, originY);
 
-					var left = elmFrom.offsetLeft;
-					var top = elmFrom.offsetTop;
-					var width = elmFrom.offsetWidth;
-					var height = elmFrom.offsetHeight;
+const canvasTop = canvasElm.getBoundingClientRect().top;
+const canvasLeft = canvasElm.getBoundingClientRect().left;
 
-					drawFromX = drawToX = offsetCenterLeft = left + (width / 2);
-					drawFromY = drawToY = offsetCenterTop = top + (height / 2);
+var fromId = null;
+var toId = null;
+var drawFrom = new Point(null, null);
+var drawTo = new Point(null, null);
+var dict = Directions.NONE;
+var isFixed = false;
 
-					console.log(left + " " + top);
-				}
+function startDraw(e) {
 
-				function draw(e) {
+	e.preventDefault();
 
+	console.log("started");
 
-					e.preventDefault();
+	var destX = e.touches[0].pageX;
+	var destY = e.touches[0].pageY;
 
+	var elmFrom = document.elementFromPoint(destX, destY);
+	fromId = elmFrom.dataset.id;
 
+	var left = elmFrom.offsetLeft;
+	var top = elmFrom.offsetTop;
+	var width = elmFrom.getBoundingClientRect().width;
+	var height = elmFrom.getBoundingClientRect().height;
 
-					console.log("drawing");
 
-					var destX = e.changedTouches[0].pageX;
-					var destY = e.changedTouches[0].pageY;
 
-					var elmFrom = document.elementFromPoint(destX, destY);
+	var drawFromX = offsetCenterLeft = left + (width / 2);
+	var drawFromY = offsetCenterTop = top + (height / 2);
 
-					var left = elmFrom.offsetLeft;
-					var top = elmFrom.offsetTop;
-					var width = elmFrom.offsetWidth;
-					var height = elmFrom.offsetHeight;
+	drawFrom.setX = drawFromX;
+	drawFrom.setY = drawFromY;
+	drawTo.setX = drawFromX;
+	drawTo.setY = drawFromY;
 
-					drawToX = left + (width / 2);
-					drawToY = top + (height / 2);
+	console.log(left + " " + top);
+}
 
-					console.log(drawFromX + " " + drawFromY);
-					console.log(drawToX + " " + drawToY);
-					console.log(width + " : width");
-					console.log(height + " : height");
+function draw(e) {
 
-					var element = document.getElementsByClassName( "board-canvas" )[0];
-					console.log(element);
-					var context = element.getContext( "2d" ) ;
 
+	e.preventDefault();
 
-					console.log(context);
+	console.log("drawing");
 
 
-					context.fillStyle = '#aaa';
-					context.strokeStyle = '#aaa';
-					context.beginPath() ;
-					context.lineCap = "round";
+	var destX = e.changedTouches[0].pageX;
+	var destY = e.changedTouches[0].pageY;
 
-					context.moveTo( 19, 19 );
-					context.lineTo( 105, 19 );
 
-					context.strokeStyle = "red" ;
-					context.lineWidth = 30 ;
-					context.strokeStyle = 'rgb(255,0,255)';
-					context.stroke() ;
+	var elmTo = document.elementsFromPoint(destX, destY)[0];
 
+	toId = elmTo.dataset.id;
 
-				}
 
-				function endDraw(e) {
-					console.log("ended");
+	if (elmTo.tagName == 'SPAN' && isDraggable(fromId, toId)) {
+		console.log("fromId:" + fromId + " toId" + toId);
+		var left = elmTo.offsetLeft;
+		var top = elmTo.offsetTop;
+		var width = elmTo.getBoundingClientRect().width;
+		var height = elmTo.getBoundingClientRect().height;
 
+		var drawToX = left + (width / 2);
+		var drawToY = top + (height / 2);
 
-				}
+		drawTo.setX = drawToX;
+		drawTo.setY = drawToY;
 
-				var letters = document.getElementsByClassName('letter');
+		var element = document.getElementsByClassName("board-canvas")[0];
+		var context = element.getContext("2d");
 
-				for (var i = 0; i < letters.length; i++) {
-					console.log("i = " + i);
-					letters[i].ontouchstart = startDraw;
-					letters[i].ontouchmove = draw;
-					letters[i].ontouchend = endDraw;
-				}
+		context.clearRect(0, 0, canvasElm.width, canvasElm.height)
 
-				/**
-					var from;
-					var to;
+		context.fillStyle = '#aaa';
+		context.strokeStyle = '#aaa';
+		context.beginPath();
+		context.lineCap = "round";
 
+		context.moveTo(drawFrom.getX, drawFrom.getY);
+		context.lineTo(drawTo.getX, drawTo.getY);
 
-					$('.letter').on('touchstart', function() {
+		context.strokeStyle = "red";
+		context.lineWidth = 30;
+		context.strokeStyle = 'rgb(255,0,255)';
+		context.stroke();
 
-						console.log("touchstart");
+	}
+}
 
-						from = to = $(this).data('id');
+function endDraw(e) {
+	console.log("ended");
+}
 
-						var context = $('canvas')[0].getContext('2d');
-						//ここに具体的な描画内容を指定する
-						//新しいパスを開始する
-						context.beginPath();
-						//パスの開始座標を指定する
+var letters = document.getElementsByClassName('letter');
 
-						var left = $(this).offset().left;
-						var top = $(this).offset().top;
-						var width = $(this).width();
-						var height = $(this).height();
+for (var i = 0; i < letters.length; i++) {
+	console.log("i = " + i);
+	letters[i].ontouchstart = startDraw;
+	letters[i].ontouchmove = draw;
+	letters[i].ontouchend = endDraw;
+}
 
-						offsetCenterLeft = left + (width / 2);
-						offsetCenterTop = top + (height / 2);
-
-						context.moveTo(offsetCenterLeft, offsetCenterTop);
-
-						console.log(from);
-					});
-
-
-
-					$('.letter').on('touchmove', function() {
-						console.log("touchmove");
-
-						to = $(this).data('id');
-						console.log(to);
-
-						var context = $('canvas')[0].getContext('2d');
-						//ここに具体的な描画内容を指定する
-						//新しいパスを開始する
-
-						var left = $(this).offset().left;
-						var top = $(this).offset().top;
-						var width = $(this).width();
-						var height = $(this).height();
-
-						offsetCenterLeft = left + (width / 2);
-						offsetCenterTop = top + (height / 2);
-
-						context.fillStyle = '#ccc';
-						context.strokeStyle = 'red';
-
-						context.lineTo(offsetCenterLeft, offsetCenterTop);
-						context.stroke();
-					});
-
-					$('.letter').on('touchend', function() {
-				/*
-						console.log("touchend");
-				 */
-
-				/**
-				 var context = $('canvas')[0].getContext('2d');
-				 //ここに具体的な描画内容を指定する
-				 //新しいパスを開始する
-
-				 var left = $(this).offset().left;
-				 var top = $(this).offset().top;
-				 var width = $(this).width();
-				 var height = $(this).height();
-
-				 offsetCenterLeft = left + (width / 2);
-				 offsetCenterTop = top + (height / 2);
-
-				 context.fillStyle = '#ccc';
-				 context.strokeStyle = 'red';
-				 /**
-				 console.log(offsetCenterLeft + " " + offsetCenterTop);
-				 **/
-
-				/**
-				 context.lineTo(offsetCenterLeft, offsetCenterTop);
-				 context.stroke();
-				 /**
-				 console.log("from : " + from.attr('data-id') + " to : " + to.attr('data-id'));
-				 **/
-				/*	});*/
