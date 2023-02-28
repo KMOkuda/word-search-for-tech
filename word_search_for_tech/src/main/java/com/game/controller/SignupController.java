ed40d012a9b5380767d1303bc094db4d1c9ec39e
KMOkuda <kyo.trainee.sys.202205@gmail.com>
2023-02-28 16:57:43 +0900

    次やること：フォーム完成させる。ログイン機能つける。

--- word_search_for_tech/src/main/java/com/game/controller/SignupController.java
+++ word_search_for_tech/src/main/java/com/game/controller/SignupController.java
@@ -1,17 +1,21 @@
 package com.game.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class SignupController {
+
 	@GetMapping("/signup")
 	public String getLogin(Model model) {
-		return "signup";
+		model.addAttribute("contents", "signup/signup::signup_contents");
+		return "layout";
 	}
 
 	@PostMapping("/signup")
 	public String postSignup(Model model) {
-		return "signup";
+		return "layout";
 	}
 }
